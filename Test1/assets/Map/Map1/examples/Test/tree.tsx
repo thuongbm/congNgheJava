8bb358638e2cad25c99a32283c48b461ddca2315
thuong <buiminhthuong30122005@gmail.com>
2025-03-09 02:25:14 +0700

    cập nhật thêm bản đồ vào, đặt tầm nhìn theo đối tượng

---
+++ Test1/assets/Map/Map1/examples/Test/tree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tree" tilewidth="32" tileheight="32" tilecount="169" columns="13">
+ <image source="../Download/plant repack.png" width="416" height="416"/>
+</tileset>
--- Test1/core/src/main/java/io/github/some_snake_name/Main1.java
+++ Test1/core/src/main/java/io/github/some_snake_name/Main1.java
@@ -7,82 +7,112 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.assets.AssetManager;
 
 public class Main1 extends ApplicationAdapter {
     private ShapeRenderer shapeRenderer; // bien tao hinh vuong
-    private FitViewport viewport; // view khi chay
-    private float posX; // vi tri theo truc ngang
-    private float posY; // vi tri theo truc doc
-    private float speed = 100; // toc do o vuong
-    private int dirX = -1;
-    private int dirY = 0;
+    private FitViewport viewport; // view
+    private float posX, posY; //vi tri
+    private float speed = 100; // toc do
+    private int dirX = -1, dirY = 0; // huong
+    private AssetManager assetManager;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer mapRenderer; // lay ban do tu project tiled
+    private OrthographicCamera camera; // goc cam nguoi choi
 
     @Override
     public void create() {
-        viewport = new FitViewport(800, 600); // dat cua so dai 800 rong 600
-        viewport.getCamera().position.set(400, 300, 0);
-        viewport.getCamera().update();
-
-        shapeRenderer = new ShapeRenderer();
-        posX = 300; // vi tri ban dau
+        shapeRenderer = new ShapeRenderer(); // hinh vuong
+        posX = 300;
         posY = 300;
-        dirX = -1;
+
+        // Load bản đồ
+        assetManager = new AssetManager();
+        assetManager.setLoader(TiledMap.class, new TmxMapLoader());
+        assetManager.load("Map/Map1/examples/Test/ran1.tmx", TiledMap.class);
+        assetManager.finishLoading();
+        map = assetManager.get("Map/Map1/examples/Test/ran1.tmx", TiledMap.class); // dat ban do trong bien map
+
+        // Lấy kích thước bản đồ
+        TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(0);
+        int mapWidth = layer.getWidth() * 32; // lay rong
+        int mapHeight = layer.getHeight() * 32; // lay chieu cao
+
+        // Khởi tạo camera và viewport
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(mapWidth, mapHeight, camera);
+        camera.position.set(mapWidth / 2f, mapHeight / 2f, 0);
+        camera.update();
+
+        // Tạo renderer
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(Color.BLACK); // mau nen cho cua so
+        ScreenUtils.clear(Color.BLACK);
         viewport.apply();
 
-        input(); // goi ham
-//
-//        if (posX + 200 <= 0) {
-//            posX = 800;
-//        }
-//
-//        if (posX + 200 >= -200) {
-//            posX = 0;
-//        }
-        posX += dirX * speed * Gdx.graphics.getDeltaTime(); // Luôn di chuyển theo hướng hiện tại
-        posY += dirY * speed * Gdx.graphics.getDeltaTime();
-//
-        shapeRenderer.setProjectionMatrix(viewport.getCamera().combined); // bat dau ve
+        // Cập nhật camera
+        camera.position.set(posX, posY, 0);
+        camera.update();
+        mapRenderer.setView(camera);
+        mapRenderer.render(); // Đưa map lên
 
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled); // tao hinh vuong dac
-        shapeRenderer.setColor(Color.RED); // dat mau
-        shapeRenderer.rect(posX, posY, 10, 10);// kich co vi tri dat hinh vuong ban dau
-        shapeRenderer.end(); // ket thuc ve
+        input();
+
+        // Cập nhật vị trí nhân vật
+        posX += dirX * speed * Gdx.graphics.getDeltaTime();
+        posY += dirY * speed * Gdx.graphics.getDeltaTime();
+
+        // vẽ hình vuông lên bản đồ
+        shapeRenderer.setProjectionMatrix(camera.combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.RED);
+        shapeRenderer.rect(posX, posY, 10, 10);
+        shapeRenderer.end();
     }
+
     private void input() {
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && dirX == 0) {
             dirX = 1;
             dirY = 0;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            dirX = -1; // sang trais
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && dirX == 0) {
+            dirX = -1;
             dirY = 0;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) && dirY == 0) {
             dirX = 0;
-            dirY = 1; // lên
+            dirY = 1;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && dirY == 0) {
             dirX = 0;
-            dirY = -1; // xuống
+            dirY = -1;
         }
     }
+
+    // Cập nhật kích thước cửa khi cửa sổ thay đổi
     @Override
     public void resize(int width, int height) {
         viewport.update(width, height, true);
     }
 
+    //Dọn dẹp bộ nhớ khi thoát game
     @Override
     public void dispose() {
         shapeRenderer.dispose();
+        if (map != null) map.dispose();
+        if (mapRenderer != null) mapRenderer.dispose();
+        if (assetManager != null) assetManager.dispose();
     }
 
     public static void main(String[] args) {
@@ -91,5 +121,4 @@ public class Main1 extends ApplicationAdapter {
         config.setWindowedMode(640, 640);
         new Lwjgl3Application(new Main1(), config);
     }
-
 }
