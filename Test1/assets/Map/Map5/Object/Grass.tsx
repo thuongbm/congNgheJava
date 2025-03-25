5e32ca059617c929d337c8473b349fa900a8c448
thuong <buiminhthuong30122005@gmail.com>
2025-03-25 10:31:08 +0700

    add map 3, 4, 5

---
+++ Test1/assets/Map/Map5/Object/Grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Grass" tilewidth="8" tileheight="8" tilecount="900" columns="30">
+ <image source="../Download/grasstileset.png" width="240" height="240"/>
+</tileset>
